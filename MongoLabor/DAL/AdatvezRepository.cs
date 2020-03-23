f39213242cea4d5c06bef6055deeda529b15a870
levoskaa <laurinyecz.levi@gmail.com>
2020-03-23 20:29:49 +0100

    Feladat 1

--- MongoLabor/DAL/AdatvezRepository.cs
+++ MongoLabor/DAL/AdatvezRepository.cs
@@ -1,40 +1,78 @@
-﻿using MongoDB.Driver;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
 using MongoLabor.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MongoLabor.DAL
 {
     public class AdatvezRepository : IAdatvezRepository
     {
+        private readonly IMongoCollection<Entities.Termek> termekCollection;
+
         public AdatvezRepository(IMongoDatabase database)
         {
-            // TODO
+            this.termekCollection = database.GetCollection<Entities.Termek>("termekek");
         }
 
         public IList<Termek> ListTermekek()
         {
-            throw new NotImplementedException();
+            var dbTermekek = termekCollection
+                .Find(_ => true)
+                .ToList();
+            return dbTermekek
+                .Select(t => new Termek
+                {
+                    ID = t.ID.ToString(),
+                    Nev = t.Nev,
+                    NettoAr = t.NettoAr,
+                    Raktarkeszlet = t.Raktarkeszlet
+                })
+                .ToList();
         }
 
         public Termek FindTermek(string id)
         {
-            throw new NotImplementedException();
+            var dbTermek = termekCollection
+                .Find(t => t.ID == ObjectId.Parse(id))
+                .SingleOrDefault();
+            if (dbTermek == null)
+                return null;
+            return  new Termek
+                {
+                    ID = dbTermek.ID.ToString(),
+                    Nev = dbTermek.Nev,
+                    NettoAr = dbTermek.NettoAr,
+                    Raktarkeszlet = dbTermek.Raktarkeszlet
+                };
         }
 
         public void InsertTermek(Termek termek)
         {
-            throw new NotImplementedException();
+            var dbTermek = new Entities.Termek
+            {
+                Nev = termek.Nev,
+                NettoAr = termek.NettoAr,
+                Raktarkeszlet = termek.Raktarkeszlet,
+                AFA = new Entities.AFA { Nev = "Általános", Kulcs = 20 },
+                KategoriaID = ObjectId.Parse("5d7e42adcffa8e1b64f7dbc1")
+            };
+            termekCollection.InsertOne(dbTermek);
         }
 
         public bool TermekElad(string id, int mennyiseg)
         {
-            throw new NotImplementedException();
+            var result = termekCollection.UpdateOne(
+                filter: t => t.ID == ObjectId.Parse(id) && t.Raktarkeszlet >= mennyiseg,
+                update: Builders<Entities.Termek>.Update.Inc(t => t.Raktarkeszlet, -mennyiseg),
+                options: new UpdateOptions { IsUpsert = false });
+            return result.MatchedCount > 0;
         }
 
         public void DeleteTermek(string id)
         {
-            throw new NotImplementedException();
+            termekCollection.DeleteOne(t => t.ID == ObjectId.Parse(id));
         }
 
         public IList<Kategoria> ListKategoriak()
